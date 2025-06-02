1e7c44bf920923af23f50375945170153618d4bf
unknown <caiofmenezes@gmail.com>
2025-06-02 04:05:17 -0300

    Mesclando alterações locais com o repositório remoto

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,6 +1,6 @@
 // @ts-check
 // Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
+// sayCain
 
 const { SpecReporter, StacktraceOption } = require('jasmine-spec-reporter');
 
